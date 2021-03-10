b8104fa4d058c4a485299506eef4ad8965628586
93Kamuran <enginkamuran@gmail.com>
2021-03-10 15:19:12 +0300

    SmsProcessor job added to SmsNotifier command.

--- src/Console/Commands/SteamaSmsNotifier.php
+++ src/Console/Commands/SteamaSmsNotifier.php
@@ -1,19 +1,19 @@
 <?php
 
-
 namespace Inensus\SteamaMeter\Console\Commands;
 
+use App\Jobs\SmsProcessor;
 use App\Models\Sms;
 use App\Sms\SmsTypes;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Inensus\SteamaMeter\Helpers\SmsBodyGenerator;
+use Inensus\SteamaMeter\Jobs\SteamaSmsProcessor;
 use Inensus\SteamaMeter\Services\SteamaCustomerService;
 use Inensus\SteamaMeter\Services\SteamaSmsNotifiedCustomerService;
 use Inensus\SteamaMeter\Services\SteamaSmsSettingService;
 use Inensus\SteamaMeter\Services\SteamaTransactionsService;
+use Inensus\SteamaMeter\Sms\SteamaSmsTypes;
 use Inensus\StemaMeter\Exceptions\CronJobException;
-use Webpatser\Uuid\Uuid;
 
 class SteamaSmsNotifier extends Command
 {
@@ -25,15 +25,13 @@ class SteamaSmsNotifier extends Command
     private $steamaTransactionService;
     private $steamaSmsNotifiedCustomerService;
     private $steamaCustomerService;
-    private $smsBodyGenerator;
 
     public function __construct(
         SteamaSmsSettingService $smsSettingService,
         Sms $sms,
         SteamaTransactionsService $steamaTransactionsService,
         SteamaSmsNotifiedCustomerService $steamaSmsNotifiedCustomerService,
-        SteamaCustomerService $steamaCustomerService,
-        SmsBodyGenerator $smsBodyGenerator
+        SteamaCustomerService $steamaCustomerService
     ) {
         parent::__construct();
         $this->smsSettingsService = $smsSettingService;
@@ -41,7 +39,6 @@ class SteamaSmsNotifier extends Command
         $this->steamaTransactionService = $steamaTransactionsService;
         $this->steamaSmsNotifiedCustomerService = $steamaSmsNotifiedCustomerService;
         $this->steamaCustomerService = $steamaCustomerService;
-        $this->smsBodyGenerator = $smsBodyGenerator;
     }
 
     public function handle()
@@ -52,14 +49,19 @@ class SteamaSmsNotifier extends Command
         $startedAt = Carbon::now()->toIso8601ZuluString();
         $this->info('smsNotifier command started at ' . $startedAt);
         try {
-
             $smsSettings = $this->smsSettingsService->getSmsSettings();
-            $transactionMin = $smsSettings->where('state', 'Transactions')->first()->not_send_elder_than_mins;
-            $lowBalanceMin = $smsSettings->where('state', 'Low Balance Warning')->first()->not_send_elder_than_mins;
+            $transactionMin = $smsSettings->where('state', 'Transactions')
+                ->first()->not_send_elder_than_mins;
+            $lowBalanceMin = $smsSettings->where('state', 'Low Balance Warning')
+                ->first()->not_send_elder_than_mins;
             $smsNotifiedCustomers = $this->steamaSmsNotifiedCustomerService->getSteamaSmsNotifiedCustomers();
-            $customers = $this->steamaCustomerService->getSteamaCustomersWithAddress($lowBalanceMin);
+            $customers = $this->steamaCustomerService->getSteamaCustomersWithAddress();
             $this->sendTransactionNotifySms($transactionMin, $smsNotifiedCustomers, $customers);
-            $this->sendLowBalanceWarningNotifySms($customers, $smsNotifiedCustomers, $lowBalanceMin);
+            $this->sendLowBalanceWarningNotifySms($customers->where(
+                'updated_at',
+                '>=',
+                Carbon::now()->subMinutes($lowBalanceMin)
+            )->get(), $smsNotifiedCustomers, $lowBalanceMin);
         } catch (CronJobException $e) {
             $this->warn('dataSync command is failed. message => ' . $e->getMessage());
         }
@@ -71,76 +73,69 @@ class SteamaSmsNotifier extends Command
 
     private function sendTransactionNotifySms($transactionMin, $smsNotifiedCustomers, $customers)
     {
-        $this->steamaTransactionService->getSteamaTransactions($transactionMin)->each(function ($steamaTransaction) use
-        (
-            $transactionMin,
-            $smsNotifiedCustomers,
-            $customers
-        ) {
+        $this->steamaTransactionService->getSteamaTransactions($transactionMin)
+            ->each(function ($steamaTransaction) use ($transactionMin, $smsNotifiedCustomers, $customers) {
+                $smsNotifiedCustomers = $smsNotifiedCustomers->where(
+                    'notify_id',
+                    $steamaTransaction->id
+                )->where('customer_id', $steamaTransaction->customer_id)->first();
+                if ($smsNotifiedCustomers) {
+                    return true;
+                }
+                $notifyCustomer = $customers->filter(function ($customer) use ($steamaTransaction) {
+                    return $customer->customer_id == $steamaTransaction->customer_id;
+                })->first();
 
-            $smsNotifiedCustomers = $smsNotifiedCustomers->where('notify_id',
-                $steamaTransaction->id)->where('customer_id', $steamaTransaction->customer_id)->first();
-            if ($smsNotifiedCustomers) {
-                return true;
-            }
-            $notifyCustomer = $customers->filter(function ($customer) use ($steamaTransaction) {
-                return $customer->customer_id == $steamaTransaction->customer_id;
-            })->first();
+                if (!$notifyCustomer) {
+                    return true;
+                }
 
-            if (!$notifyCustomer) {
+                if (
+                    !$notifyCustomer->mpmPerson->addresses ||
+                    $notifyCustomer->mpmPerson->addresses[0]->phone === null ||
+                    $notifyCustomer->mpmPerson->addresses[0]->phone === ""
+                ) {
+                    return true;
+                }
+                SmsProcessor::dispatch(
+                    $steamaTransaction->thirdPartyTransaction->transaction,
+                    SmsTypes::TRANSACTION_CONFIRMATION
+                )->allOnConnection('redis')->onQueue(\config('services.queues.sms'));
 
-                return true;
-            }
-
-            if (!$notifyCustomer->mpmPerson->addresses || $notifyCustomer->mpmPerson->addresses[0]->phone === null ||
-                $notifyCustomer->mpmPerson->addresses[0]->phone === "") {
-                return true;
-            }
-
-            $sms = new Sms();
-            $sms->uuid = (string)Uuid::generate(4);
-            resolve('SmsProvider')
-                ->sendSms(
-                    $steamaTransaction->thirdPartyTransaction->transaction->sender,
-                    $this->smsBodyGenerator->generateSmsBody($steamaTransaction->thirdPartyTransaction->transaction,
-                        $notifyCustomer),
-                    sprintf(config()->get('services.sms.callback'), $sms->uuid)
+                $this->steamaSmsNotifiedCustomerService->createTransactionSmsNotify(
+                    $notifyCustomer->customer_id,
+                    $steamaTransaction->id
                 );
-
-            $this->steamaSmsNotifiedCustomerService->createTransactionSmsNotify($notifyCustomer->customer_id,
-                $steamaTransaction->id);
-            return true;
-        });
-
+                return true;
+            });
     }
 
     private function sendLowBalanceWarningNotifySms($customers, $smsNotifiedCustomers, $lowBalanceMin)
     {
-
         $customers->each(function ($customer) use (
             $smsNotifiedCustomers,
             $lowBalanceMin
         ) {
-            $notifiedCustomer = $smsNotifiedCustomers->where('notify_type','low_balance')->where('customer_id',
-                $customer->customer_id)->first();
+            $notifiedCustomer = $smsNotifiedCustomers->where('notify_type', 'low_balance')->where(
+                'customer_id',
+                $customer->customer_id
+            )->first();
             if ($notifiedCustomer) {
                 return true;
             }
-            if ($customer->account_balance>$customer->low_balance_warning){
+            if ($customer->account_balance > $customer->low_balance_warning) {
                 return true;
             }
-            if (!$customer->mpmPerson->addresses || $customer->mpmPerson->addresses[0]->phone === null ||
-                $customer->mpmPerson->addresses[0]->phone === "") {
+            if (
+                !$customer->mpmPerson->addresses || $customer->mpmPerson->addresses[0]->phone === null ||
+                $customer->mpmPerson->addresses[0]->phone === ""
+            ) {
                 return true;
             }
-            $sms = new Sms();
-            $sms->uuid = (string)Uuid::generate(4);
-            resolve('SmsProvider')
-                ->sendSms(
-                    $customer->mpmPerson->addresses[0]->phone,
-                    $this->smsBodyGenerator->generateSmsBody(null, $customer),
-                    sprintf(config()->get('services.sms.callback'), $sms->uuid)
-                );
+            SteamaSmsProcessor::dispatch(
+                $customer,
+                SteamaSmsTypes::LOW_BALANCE_LIMIT_NOTIFIER
+            )->allOnConnection('redis')->onQueue(\config('services.queues.sms'));
 
             $this->steamaSmsNotifiedCustomerService->createLowBalanceSmsNotify($customer->customer_id);
             return true;
