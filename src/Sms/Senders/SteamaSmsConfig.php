dd9351e025911b7afdf9f89ad06359eb72f5e757
93Kamuran <enginkamuran@gmail.com>
2021-04-07 11:20:20 +0300

    New sms types added.

--- src/Sms/Senders/SteamaSmsConfig.php
+++ src/Sms/Senders/SteamaSmsConfig.php
@@ -9,6 +9,7 @@ class SteamaSmsConfig extends SmsConfigsCore
 {
     public $smsTypes = [
         SteamaSmsTypes::LOW_BALANCE_LIMIT_NOTIFIER => 'Inensus\SteamaMeter\Sms\Senders\LowBalanceLimitNotifier',
+        SteamaSmsTypes::BALANCE_FEEDBACK => 'Inensus\SteamaMeter\Sms\Senders\BalanceFeedback',
     ];
     public $bodyParsersPath = 'Inensus\\SteamaMeter\\Sms\\BodyParsers\\';
     public $servicePath = 'Inensus\SteamaMeter\Services\SteamaSmsBodyService';
--- src/Sms/SteamaSmsTypes.php
+++ src/Sms/SteamaSmsTypes.php
@@ -5,4 +5,5 @@ namespace Inensus\SteamaMeter\Sms;
 class SteamaSmsTypes
 {
     public const LOW_BALANCE_LIMIT_NOTIFIER = 1;
+    public const BALANCE_FEEDBACK = 2;
 }
